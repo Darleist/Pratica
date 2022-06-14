6d6dae715c8dd42d6292f028f7f638234ce2cc91
Darlei Strege <darleistrege.py@gmail.com>
2022-06-14 09:45:14 -0400

    Funcionou!!!!

--- pratica_002/script.js
+++ pratica_002/script.js
@@ -2,13 +2,14 @@ let randomStringContainer = document.querySelector('.random_string')
 
 const  generateRandomString = (num) => {
     const characters ='abcdefghijklmnopqrstuvwxyz';
-    let result1= ' ';
+    let result1= '';
     const charactersLength = characters.length;
     for ( let i = 0; i < num; i++ ) {
         result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result1;
 }
+
 
 const isPalindromo = (n) =>{
     let txt = n.split('').reverse().join('')
@@ -17,14 +18,11 @@ const isPalindromo = (n) =>{
 }
 
 const displayRandomString =() =>{
-   
-    for(c=0; c<10; c++){
-        let res= generateRandomString(1)
-        if (isPalindromo(res) == true) randomStringContainer.innerHTML += res
-     //   else {randomStringContainer.innerHTML += res}
+    for(c=0; c<1000; c++){
+        let res= generateRandomString(4)
+        if (isPalindromo(res) == true) randomStringContainer.innerHTML += `A palavra <b>${res}</b> é palindroma <br>`
         console.log(res)
     }
-    
     }
 
 displayRandomString()
